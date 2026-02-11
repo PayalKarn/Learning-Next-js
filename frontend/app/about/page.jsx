e848fdbdbce93f68e7d43ae9e56b6e5893947616
payal <payalkarn2410@gmail.com>
2026-02-11 15:46:42 +0530

    Routing in Next js

---
+++ frontend/app/about/page.jsx
@@ -0,0 +1,4 @@
+const About = async () => {
+  return <h1>Hii this is About Page</h1>;
+};
+export default About;
---
+++ frontend/app/service/page.jsx
@@ -0,0 +1,4 @@
+const Service = async()=>{
+    return <h1>Hello this is my service page </h1>
+}
+export default Service
