73548b5bdabf4a79b243d7e2a225a03fa0fc09b9
payal <payalkarn2410@gmail.com>
2026-02-11 15:59:29 +0530

    Nested layout in next js

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,7 +1,10 @@
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>{children}</body>
+
+      <body>
+        <h1>This is my layout screen</h1>
+        {children}</body>
     </html>
   );
 }
---
+++ frontend/app/service/layout.jsx
@@ -0,0 +1,10 @@
+export default function ServiceLayout({ children }) {
+  return (
+    <html>
+      <body>
+        <h1>This is my about service layout screen</h1>
+        {children}
+      </body>
+    </html>
+  );
+}
